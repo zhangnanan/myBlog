20e527a8f6ec272ccc5db3ed4900558858d2d991
Nan Zhang <znanzh@cn.ibm.com>
2020-07-17 13:15:42 +0800

    first commit

---
+++ .vuepress/config.js
@@ -0,0 +1,108 @@
+module.exports = {
+  "title": "zhangnan's blog",
+  "description": "zhangnan's 个人博客",
+  "dest": "public",
+  "head": [
+    [
+      "link",
+      {
+        "rel": "icon",
+        "href": "/favicon.ico"
+      }
+    ],
+    [
+      "meta",
+      {
+        "name": "viewport",
+        "content": "width=device-width,initial-scale=1,user-scalable=no"
+      }
+    ]
+  ],
+  "theme": "reco",
+  "themeConfig": {
+    "nav": [
+      {
+        "text": "Home",
+        "link": "/",
+        "icon": "reco-home"
+      },
+      {
+        "text": "TimeLine",
+        "link": "/timeline/",
+        "icon": "reco-date"
+      },
+      {
+        "text": "Docs",
+        "icon": "reco-message",
+        "items": [
+          {
+            "text": "vuepress-reco",
+            "link": "/docs/theme-reco/"
+          }
+        ]
+      },
+      {
+        "text": "Contact",
+        "icon": "reco-message",
+        "items": [
+          {
+            "text": "GitHub",
+            "link": "https://github.com/zhangnanan",
+            "icon": "reco-github"
+          }
+        ]
+      }
+    ],
+    "plugins": {
+        "vuepress-plugin-auto-sidebar": {}
+    },
+    // "plugins": [
+    //   [
+    //     "@vuepress-reco/vuepress-plugin-kan-ban-niang",
+    //     {
+    //       "theme": ["miku"],
+    //     }
+    //   ]
+    // ],
+
+    "sidebar": {
+      "/docs/theme-reco/": [
+        "",
+        "theme",
+        "plugin",
+        "api"
+      ]
+    },
+    "type": "blog",
+    "blogConfig": {
+      "category": {
+        "location": 2,
+        "text": "Category"
+      },
+      "tag": {
+        "location": 3,
+        "text": "Tag"
+      }
+    },
+    "friendLink": [
+      
+      {
+        "title": "vuepress-theme-reco",
+        "desc": "A simple and beautiful vuepress Blog & Doc theme.",
+        "avatar": "https://vuepress-theme-reco.recoluan.com/icon_vuepress_reco.png",
+        "link": "https://vuepress-theme-reco.recoluan.com"
+      }
+    ],
+    "logo": "/logo.png",
+    "search": true,
+    "searchMaxSuggestions": 10,
+    "lastUpdated": "Last Updated",
+    "author": "zhangnan",
+    "authorAvatar": "/avatar.png",
+    "record": "xxxx",
+    "startYear": "2020"
+  },
+  "markdown": {
+    "lineNumbers": true
+  }
+}
